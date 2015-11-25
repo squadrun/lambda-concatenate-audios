d48827750c0f8ed4ed36fa968b0e933b9cec39fb
Ketan Bhatt <ketan@squadrun.co>
2015-11-25 11:53:02 +0530

    Fix conversion of list as string to list

--- aggregate_audios.py
+++ aggregate_audios.py
@@ -1,15 +1,17 @@
 import urllib2
 import boto3
+import ast
 
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    urls = event['urls']
+    urls = ast.literal_eval(event['urls'])  # converts list as string to list
     key = event['filename']
     bucket = event['bucket']
     aggregated_mp3s = ""  # would store audio bytes as string
-    
+        
     for url in urls:
+        print url
         try:
             aggregated_mp3s += urllib2.urlopen(url).read()  # concatenating mp3s here
         except urllib2.URLError as e:
