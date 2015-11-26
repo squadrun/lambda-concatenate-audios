7efeb19a5eb91b50907cf334e07239eb04edd509
Ketan Bhatt <ketan@squadrun.co>
2015-11-26 17:34:53 +0530

    Update aggregate_audios.py

--- aggregate_audios.py
+++ aggregate_audios.py
@@ -1,21 +1,24 @@
 import urllib2
 import boto3
-import ast
 
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    urls = ast.literal_eval(event['urls'])  # converts list as string to list
+    urls = event['urls']  # converts list as string to list
     key = event['filename']
     bucket = event['bucket']
     aggregated_mp3s = ""  # would store audio bytes as string
-        
+    
+    print urls
+    
     for url in urls:
         print url
         try:
             aggregated_mp3s += urllib2.urlopen(url).read()  # concatenating mp3s here
         except urllib2.URLError as e:
-            pass
+            print e.reason
+        except ValueError as e:
+            print "Not a Valid URL"
     
     response = s3.put_object(
             ACL='public-read',
